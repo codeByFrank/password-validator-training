7c7bfdd44c418af70d7b9b06f2f1e74b5d3bcad5
codeByFrank <119358990+codeByFrank@users.noreply.github.com>
2026-09-22 13:32:24 +0200

    Add password validator method signatures with a new class

---
+++ src/main/java/de/neuefische/PasswordValidator.java
@@ -0,0 +1,24 @@
+package de.neuefische;
+
+public final class PasswordValidator {
+
+    public static boolean hasMinLength(String password, int min) {
+        return false;
+    }
+
+    public static boolean containsDigit(String password) {
+        return false;
+    }
+
+    public static boolean containsUpperAndLower(String password) {
+        return false;
+    }
+
+    public static boolean isCommonPassword(String password) {
+        return false;
+    }
+
+    public static boolean isValid(String password) {
+        return false;
+    }
+}
