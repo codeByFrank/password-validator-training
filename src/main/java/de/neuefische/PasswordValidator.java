09dc0cbaaad62737a43cf2670cbd23e9326d8d6e
codeByFrank <119358990+codeByFrank@users.noreply.github.com>
2026-09-22 15:45:19 +0200

    Implement special character validation

--- src/main/java/de/neuefische/PasswordValidator.java
+++ src/main/java/de/neuefische/PasswordValidator.java
@@ -95,6 +95,21 @@ public final class PasswordValidator {
 
     // Bonus
     public static boolean containsSpecialChar(String password, String allowed) {
+        if (password == null || allowed == null) {
+            return false;
+        }
+
+        char[] passwordCharacters = password.toCharArray();
+        char[] allowedCharacters = allowed.toCharArray();
+
+        for (char passwordCharacter : passwordCharacters) {
+            for (char allowedCharacter : allowedCharacters) {
+                if (passwordCharacter == allowedCharacter) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }
